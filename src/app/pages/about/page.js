a1bf85872bdee3ec13c6c30b2d9f2e0345879a09
queonias <queones@gmail.com>
2024-06-08 03:56:57 -0300

    feat: card

--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -1,73 +1,34 @@
-import styles from "../../page.module.css";
 import Image from "next/image";
+import styles from "../../../styles/About.module.css";
 
 export default function About() {
     return (
-        <main className={styles.main}>
-            <div>
-
-                <h1 className={styles.title}>Sobreeeee</h1>
+        <div className={styles.about_container}>
+            <div className={styles.about_title}>
+                <h3>UM POUCO SOBRE</h3>
+                <h1>NOSSOS SERVIÇOS</h1>
             </div>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>src/app/page.js</code>
-                </p>
-                <div>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{" "}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className={styles.vercelLogo}
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
+            <div className={styles.services_container}>
+                <div className={styles.services}>
+                    <div className={styles.text_box}>
+                        <h3>BARBA</h3>
+                        <Image className={styles.imagem} src={`/images/barba.png`} width="400" height="400" />
+                    </div>
+                    <p>Na DN Barber Shop, oferecemos cortes de barba personalizados para cada cliente. Nossos barbeiros especializados garantem um estilo impecável. </p>
+                </div>
+                <div className={styles.services}>
+                    <Image className={styles.imagem} src={`/images/cabelo.png`} width="400" height="400" />
+                    <p>Na DN Barber Shop, oferecemos cortes de cabelo personalizados, cuidadosamente executados por um profissional experiente.</p>
+                </div>
+                <div className={styles.services}>
+                    <Image className={styles.imagem} src={`/images/completo.png`} width="400" height="400" />
+                    <p>Experimente o nosso pacote completo , onde você pode desfrutar de cortes de cabelo, tratamentos de barba e contornos de sobrancelha, tudo em um só lugar.</p>
+                </div>
+                <div className={styles.services}>
+                    <Image className={styles.imagem} src={`/images/domiciliar.png`} width="400" height="400" />
+                    <p>Não há necessidade de sair de casa para ter um estilo impecável. Com o nosso serviço de barbearia domiciliar, a DN Barber Shop traz o estilo até você.</p>
                 </div>
             </div>
-
-            <div className={styles.center}>
-                <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className={styles.grid}>
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Docs <span>-&gt;</span>
-                    </h2>
-                    <p>Find in-depth information about Next.js features and API.</p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Learn <span>-&gt;</span>
-                    </h2>
-                    <p>Learn about Next.js in an interactive course with quizzes!</p>
-                </a></div>
-        </main>
+        </div>
     );
 }
-
