7967cde224856d6b98a94a888217dca0709d5d0e
queonias <queones@gmail.com>
2024-06-11 19:10:02 -0300

    fix: correção de nomes das chaves

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -13,11 +13,11 @@ const {
 
 const firebaseConfig = {
     apiKey: API_KEY,
-    authDomain: AUTH_DIMAIN,
+    authDomain: AUTH_DOMAIN,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_Id,
+    appId: APP_ID,
     measurementId: MEASUREMENT_ID
 };
 
